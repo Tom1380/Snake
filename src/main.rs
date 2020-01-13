46f1988c729429159fb993760b064faeb90e0888
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2020-01-13 18:32:31 +0100

    Added support for Windows.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 use {
-    colored::*,
     getch::*,
     rand::random,
     std::{
         collections::VecDeque,
-        io::{self, Write},
+        io::Write,
         process::exit,
         sync::mpsc::{channel, Receiver, Sender},
         thread::{sleep, spawn},
         time::Duration,
     },
+    termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor},
 };
 
 const ROWS: u8 = 20;
 const COLUMNS: u8 = 20;
 const CELLS: u16 = ROWS as u16 * COLUMNS as u16;
 const MAX_SNACKS_DROPPED: u16 = CELLS / 30; // How many snacks should be dropped at a time?
-const SPEED: f32 = 0.25; // Seconds to cross a cell.
+const SPEED: f32 = 0.08; // Seconds to cross a cell.
 
 #[derive(Debug, PartialEq)]
 enum Axis {
@@ -50,7 +50,7 @@ struct Cell {
 }
 
 impl Cell {
-    fn neighbour(&self, direction: &Direction) -> Cell {
+    fn neighbour(&self, direction: &Direction, mut op: &mut OutputBuffer) -> Cell {
         use Direction::*;
         let mut neighbour = self.clone();
         match direction {
@@ -58,32 +58,70 @@ impl Cell {
                 if self.y > 0 {
                     neighbour.y -= 1;
                 } else {
-                    game_over();
+                    game_over(&mut op);
                 }
             }
             Right => {
                 if self.x < COLUMNS - 1 {
                     neighbour.x += 1;
                 } else {
-                    game_over();
+                    game_over(&mut op);
                 }
             }
             Down => {
                 if self.y < ROWS - 1 {
                     neighbour.y += 1;
                 } else {
-                    game_over();
+                    game_over(&mut op);
                 }
             }
             Left => {
                 if self.x > 0 {
                     neighbour.x -= 1;
                 } else {
-                    game_over();
+                    game_over(&mut op);
                 }
             }
         }
         neighbour
+    }
+}
+
+struct OutputBuffer {
+    buffer: Vec<(String, Option<Color>)>,
+    stdout: StandardStream,
+}
+
+impl OutputBuffer {
+    fn with_capacity(capacity: usize) -> OutputBuffer {
+        OutputBuffer {
+            buffer: Vec::with_capacity(capacity),
+            stdout: StandardStream::stdout(ColorChoice::Always),
+        }
+    }
+
+    fn append(&mut self, tup: &(String, Option<Color>)) {
+        let len = self.buffer.len();
+        if len == 0 {
+            self.buffer.push((tup.0.clone(), (tup.1)));
+            return;
+        }
+        let last = &mut self.buffer[len - 1];
+        if (tup.1) == last.1 {
+            last.0 += &tup.0;
+        } else {
+            self.buffer.push((tup.0.clone(), (tup.1)));
+        }
+    }
+
+    fn flush(&mut self) {
+        for (s, color) in &self.buffer {
+            self.stdout
+                .set_color(ColorSpec::new().set_fg(*color))
+                .unwrap();
+            write!(&mut self.stdout, "{}", s);
+        }
+        self.buffer.clear();
     }
 }
 
@@ -108,38 +146,40 @@ fn read_arrow(g: &Getch) -> Option<Direction> {
     }
 }
 
-fn print_grid(snake: &VecDeque<Cell>, snacks: &VecDeque<Cell>) {
-    let colored_fs_char = "|".color("blue");
-    let snake_char = "+".color("green");
-    let snack_char = "O".color("yellow");
-    let colored_underscore_char = "_".color("blue");
+fn print_grid(snake: &VecDeque<Cell>, snacks: &VecDeque<Cell>, op: &mut OutputBuffer) {
+    let colored_fs_char = (String::from("|"), Some(Color::Blue));
+    let snake_char = (String::from("+"), Some(Color::Green));
+    let snack_char = (String::from("O"), Some(Color::Yellow));
     for y in 0..ROWS {
         for x in 0..COLUMNS {
-            print!(
-                "{}{}",
-                colored_fs_char,
-                if snake.contains(&Cell { x, y }) {
-                    &snake_char
-                } else if snacks.contains(&Cell { x, y }) {
-                    &snack_char
-                } else {
-                    &colored_underscore_char
-                }
-            )
+            if snake.contains(&Cell { x, y }) {
+                op.append(&colored_fs_char);
+                op.append(&snake_char);
+            } else if snacks.contains(&Cell { x, y }) {
+                op.append(&colored_fs_char);
+                op.append(&snack_char);
+            } else {
+                op.append(&(String::from("|_"), Some(Color::Blue)));
+            }
         }
-        println!("{}", colored_fs_char);
+        op.append(&(String::from("|\n"), Some(Color::Blue)));
     }
-    println!("SCORE: {}", (snake.len() - 1).to_string().color("yellow"));
+    op.append(&(String::from("Score: "), Some(Color::White)));
+    op.append(&((snake.len() - 1).to_string() + &"\n", Some(Color::Yellow))); //.color("yellow"));
 }
 
-fn clear_screen() {
-    io::stdout()
-        .write_all("\x1b[2J\x1b[1;1H".as_bytes())
-        .unwrap();
+fn clear_screen(op: &mut OutputBuffer) {
+    if cfg!(target_os = "windows") {
+        op.append(&("\n".repeat(30), None));
+    }
+    else {
+        op.append(&(String::from("\x1b[2J\x1b[1;1H"), None));
+    }
 }
 
-fn game_over() {
-    println!("{}", "YOU LOST.".color("red"));
+fn game_over(op: &mut OutputBuffer) {
+    op.append(&(String::from("YOU LOST.\n"), Some(Color::Red))); //.color("red"));
+    op.flush();
     exit(0);
 }
 
@@ -179,18 +219,20 @@ fn listen_for_keys(tx: Sender<Direction>) {
 
 fn render_grid(rx: Receiver<Direction>) {
     let mut snake = vec![Cell { x: 0, y: 0 }].into_iter().collect();
+    let mut op = OutputBuffer::with_capacity(CELLS as usize + 20);
     let mut snacks = generate_snacks(&snake);
     let mut direction = rx.recv().unwrap();
     loop {
-        clear_screen();
-        print_grid(&snake, &snacks);
+        op.flush();
+        clear_screen(&mut op);
+        print_grid(&snake, &snacks, &mut op);
         sleep(Duration::from_secs_f32(SPEED));
         if let Ok(new_direction) = rx.try_recv() {
             direction = new_direction;
         }
-        let new_head = snake.iter().last().unwrap().neighbour(&direction);
+        let new_head = snake.iter().last().unwrap().neighbour(&direction, &mut op);
         if snake.contains(&new_head) {
-            game_over();
+            game_over(&mut op);
         } else if let Some(i) = snacks.iter().position(|cell| *cell == new_head) {
             snacks.remove(i);
             if snacks.is_empty() {
