5c63bdfa4172864eccd30cd4916a694faa256d7c
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2020-01-15 03:56:38 +0100

    Saved changes.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-
 use {
-    dialoguer::{theme::ColorfulTheme, Input, PasswordInput, Select},
     colored::*,
+    dialoguer::{theme::ColorfulTheme, Input, PasswordInput, Select},
+    fs::File,
     getch::*,
     rand::random,
-    fs::{File},
+    serde_json,
     std::{
+        collections::{HashMap, VecDeque},
         fs,
-        collections::{VecDeque, HashMap},
         process::exit,
         sync::mpsc::{channel, Receiver, Sender},
         thread::{sleep, spawn},
         time::Duration,
     },
-    serde_json
 };
 
 const ROWS: u8 = 20;
@@ -135,7 +134,6 @@ fn read_arrow(g: &Getch) -> Option<Direction> {
 
 fn print_grid(snake: &VecDeque<Cell>, snacks: &VecDeque<Cell>, op: &mut OutputBuffer) {
     if cfg!(target_os = "windows") {
-
         for y in 0..ROWS {
             for x in 0..COLUMNS {
                 if snake.contains(&Cell { x, y }) {
@@ -149,8 +147,6 @@ fn print_grid(snake: &VecDeque<Cell>, snacks: &VecDeque<Cell>, op: &mut OutputBu
             op.append("|\n");
         }
         op.append(format!("Score: {}\n", snake.len() - 1).as_str());
-        
-        
     } else {
         let colored_fs_char = "|".color("blue");
         let snake_char = "+".color("green");
@@ -176,11 +172,19 @@ fn print_grid(snake: &VecDeque<Cell>, snacks: &VecDeque<Cell>, op: &mut OutputBu
     }
 }
 
-fn clear_screen(op: &mut OutputBuffer) {
+fn op_clear_screen(op: &mut OutputBuffer) {
     if cfg!(target_os = "windows") {
         op.append(&"\n".repeat(30));
     } else {
         op.append("\x1b[2J\x1b[1;1H");
+    }
+}
+
+fn clear_screen() {
+    if cfg!(target_os = "windows") {
+        println!("{}", "\n".repeat(30));
+    } else {
+        print!("\x1b[2J\x1b[1;1H");
     }
 }
 
@@ -231,7 +235,7 @@ fn render_grid(rx: Receiver<Direction>) {
     let mut direction = rx.recv().unwrap();
     loop {
         op.flush();
-        clear_screen(&mut op);
+        op_clear_screen(&mut op);
         print_grid(&snake, &snacks, &mut op);
         sleep(Duration::from_secs_f32(SPEED));
         if let Ok(new_direction) = rx.try_recv() {
@@ -254,7 +258,7 @@ fn render_grid(rx: Receiver<Direction>) {
     }
 }
 
-#[derive( Debug, Clone)]
+#[derive(Debug, Clone)]
 struct User {
     username: String,
     password: String,
@@ -265,20 +269,20 @@ enum Acces {
     Denied,
 }
 // mut config: &HashMap<String,String>
-fn login(mut config : HashMap<String, String>) -> (Acces,User) {
+fn login(mut config: HashMap<String, String>) -> (Acces, User) {
     let username: String = Input::new().with_prompt("Username").interact().unwrap();
     let password: String = PasswordInput::with_theme(&ColorfulTheme::default())
         .with_prompt("Password")
         .interact()
         .unwrap();
-    
+
     config.insert("username".to_string(), username.to_owned());
     config.insert("password".to_string(), password.to_owned());
     serde_json::to_writer(&File::create("config.json").unwrap(), &config).unwrap();
-    let user = User{username,password};
-    (Acces::Granted,user)
+    let user = User { username, password };
+    (Acces::Granted, user)
 }
-fn signup() -> (Acces,User) {
+fn signup() -> (Acces, User) {
     let mut config: HashMap<String, String> = HashMap::new();
     let username: String = Input::new().with_prompt("Username").interact().unwrap();
     let password = PasswordInput::with_theme(&ColorfulTheme::default())
@@ -290,34 +294,36 @@ fn signup() -> (Acces,User) {
     config.insert("password".to_string(), password.to_owned());
     let user = User { username, password };
     serde_json::to_writer(&File::create("config.json").unwrap(), &config).unwrap();
-    (Acces::Granted,user)
+    (Acces::Granted, user)
 }
-fn main_menu(tx: std::sync::mpsc::Sender<Direction>, rx: std::sync::mpsc::Receiver<Direction>,user:User) {
-    println!("\x1b[2J\x1b[1;1H");
-    let selections = &["Play", "Settings", "Scores"];
+fn main_menu(user: User) {
+    loop {
+        clear_screen();
+        let selections = &["Play", "Settings", "Scores"];
 
-    let selection = Select::with_theme(&ColorfulTheme::default()).with_prompt(format!("Ciao, {}", user.username).as_str())
-        .default(0)
-        .items(&selections[..])
-        .interact_opt()
-        .unwrap();
+        let selection = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt(format!("Eccoti, {}", user.username).as_str())
+            .default(0)
+            .items(&selections[..])
+            .interact_opt()
+            .unwrap();
 
-    if let Some(selection) = selection {
-        match selections[selection] {
-            "Play" => {
-                spawn(move || listen_for_keys(tx));
-                render_grid(rx);
+        if let Some(selection) = selection {
+            match selections[selection] {
+                "Play" => {
+                    let (tx, rx) = channel();
+                    spawn(move || listen_for_keys(tx));
+                    render_grid(rx);
+                }
+                "Settings" => {
+                    unimplemented!();
+                }
+                "Scores" => unimplemented!(),
+                _ => unreachable!(),
             }
-            "Settings" => {
-                unimplemented!();
-            }
-            "Scores" => {
-                unimplemented!()
-            }
-            _ => unreachable!(),
+        } else {
+            println!("Non hai selezionato nulla");
         }
-    } else {
-        println!("Non hai selezionato nulla");
     }
 }
 
@@ -329,35 +335,32 @@ fn hashmapper(data: serde_json::value::Value) -> HashMap<String, String> {
     data.as_object()
         .unwrap()
         .iter()
-        .map(|(key, value)| {
-            (key.clone(), value.as_str().unwrap().to_string(),)
-        })
+        .map(|(key, value)| (key.clone(), value.as_str().unwrap().to_string()))
         .collect()
 }
 fn main() {
-    let config = read_config() ;
-    let (tx, rx) = channel();
-    // if first Acces or without account this screen will show up
+    let config = read_config();
+    // if first Access or without account this screen will show up
     let selections = &["Log-in", "Sign-up"];
 
     let hashmap = hashmapper(config);
     if !hashmap.contains_key("username") && !hashmap.contains_key("password") {
-            let selection = Select::with_theme(&ColorfulTheme::default())
-        .default(0)
-        .items(&selections[..])
-        .interact_opt()
-        .unwrap();
+        let selection = Select::with_theme(&ColorfulTheme::default())
+            .default(0)
+            .items(&selections[..])
+            .interact_opt()
+            .unwrap();
         if let Some(selection) = selection {
             match selections[selection] {
-                "Log-in" => match login( hashmap) {
-                    (Acces::Granted,user) => {
-                        main_menu(tx, rx,user);
+                "Log-in" => match login(hashmap) {
+                    (Acces::Granted, user) => {
+                        main_menu(user);
                     }
                     _ => unimplemented!(),
                 },
                 "Sign-up" => match signup() {
                     (Acces::Granted, user) => {
-                        main_menu(tx, rx,user);
+                        main_menu(user);
                     }
                     _ => unimplemented!(),
                 },
@@ -366,14 +369,11 @@ fn main() {
         } else {
             println!("Non hai selezionato nulla");
         }
-    } 
-    else {
-        let user = User{
-            username : hashmap.get("username").unwrap().to_string(),
-            password : hashmap.get("password").unwrap().to_string()
+    } else {
+        let user = User {
+            username: hashmap.get("username").unwrap().to_string(),
+            password: hashmap.get("password").unwrap().to_string(),
         };
-        main_menu(tx, rx,user);
+        main_menu(user);
     }
-
-    
 }
