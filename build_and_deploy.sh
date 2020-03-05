bec290a17f7c9cd5bd6a0c13c5cb742baf954428
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2020-03-05 03:21:07 +0100

    Added a simple deploy script.

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-docker_host="ssh://${USER}@167.172.50.64"
+DOCKER_HOST="ssh://${USER}@167.172.50.64"
 cargo build --release --target x86_64-pc-windows-gnu
 cargo build --release
-docker-compose -H "$docker_host" up --build -d
+docker-compose -H "$DOCKER_HOST" up --build -d
