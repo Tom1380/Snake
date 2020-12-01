9cd309634f086164eee371238415fd09f37c395f
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2020-12-01 22:07:54 +0100

    Pushed old stuff. I'm not actively working on this right now, so I'm pushing old changes for backup.

--- src/launcher/main.rs
+++ src/launcher/main.rs
@@ -1,15 +1,17 @@
 use {
     dialoguer::{theme::ColorfulTheme, Select},
+    game_arcade::clear_screen,
     reqwest, serde_json,
+    std::{collections::HashMap, process::Command},
 };
 
 fn main() {
     loop {
+        clear_screen();
         println!(
             "IL FOTTUTO LAUNCHER, BITCH. I TUOI CAZZO DI DEVELOPERS SONO TORNATI, NON TE L'ASPETTAVI."
         );
-        let games = read_list_of_available_games().unwrap();
-        let games = games.as_object().unwrap();
+        let games = downloadable_games();
         let selections: Vec<&String> = games.keys().collect();
         let selection = Select::with_theme(&ColorfulTheme::default())
             .default(0)
@@ -17,20 +19,30 @@ fn main() {
             .interact_opt()
             .unwrap()
             .unwrap();
-        //open_game(&games[selections[selection]]);
+        open_game(&games[selections[selection]]);
     }
 }
 
-fn read_list_of_available_games() -> Option<serde_json::Value> {
-    match reqwest::get("http://167.172.50.64/available_games") {
-        Ok(mut response) => match response.json() {
-            Ok(json) => json,
-            Err(_) => None,
-        },
-        Err(_) => None,
-    }
+fn downloadable_games() -> HashMap<String, String> {
+    reqwest::get("http://167.172.50.64/available_games")
+        .unwrap()
+        .json::<serde_json::Value>()
+        .unwrap()
+        .as_object()
+        .unwrap()
+        .iter()
+        .map(|(k, v)| (k.clone(), v.as_str().unwrap().to_string()))
+        .collect()
+}
+
+fn download_game(name: &String) {
+
 }
 
 fn open_game(name: &String) {
-
+    Command::new(format!("games/{}", name))
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap();
 }
